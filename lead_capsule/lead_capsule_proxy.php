0177dd59ef1364848c302021994365bf5d3fe142
leadgendev <leadgendev@gmail.com>
2013-06-27 17:03:14 -0400

    Beginnings of a Lead Capsule proxy PHP script

---
+++ lead_capsule/lead_capsule_proxy.php
@@ -0,0 +1,9 @@
+<?php
+/***************************************************************
+ * File: DSG_utilities/lead_capsule/lead_capsule_proxy.php
+ *
+ * Author: eamohl@leadsanddata.net
+ *
+ * Created: June 27, 2013
+ **************************************************************/
+?>
